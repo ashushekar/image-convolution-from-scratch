50eedf86004d8b896575ab637ecaa3c066ff5e69
Ashwin Naidu <ashwinnaidu1991@gmail.com>
2019-11-12 00:50:10 +0000

    second commit

--- convolve.py
+++ convolve.py
@@ -2,8 +2,11 @@
 A comprehensive tutorial towards 2D convolution and image filtering
 (The first step to understand Convolutional Neural Networks (CNNs)).
 """
+import os
 import cv2
 import numpy as np
+
+IMAGES_PATH = 'images/'
 
 def load_image(image_path):
     """
@@ -11,6 +14,9 @@ def load_image(image_path):
     :param image_path: <String> Path of input_image
     :return a numpy array of size [image_height, image_width]
     """
+    # Create the Image directory to save any plots
+    if not os.path.exists(IMAGES_PATH):
+        os.makedirs(IMAGES_PATH)
     coloured_image = cv2.imread(image_path)
     grey_image = cv2.cvtColor(coloured_image, cv2.COLOR_BGR2GRAY)
     print('image matrix size: ', grey_image.shape)
@@ -49,19 +55,19 @@ input_image = load_image('input_image.jpg')
 # kernel to be used to get sharpened image
 KERNEL = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 image_sharpen = convolve2d(input_image, kernel=KERNEL)
-cv2.imwrite('sharpened_image.jpg', image_sharpen)
+cv2.imwrite(IMAGES_PATH + 'sharpened_image.jpg', image_sharpen)
 
 # kernel to be used for edge detection
 image_edge1 = convolve2d(input_image, kernel=np.array([[-1, -1, -1], [-1, 4, -1], [-1, -1, -1]]))
-cv2.imwrite('edge_detection1.jpg', image_edge1)
+cv2.imwrite(IMAGES_PATH + 'edge_detection1.jpg', image_edge1)
 
 image_edge2 = convolve2d(input_image, kernel=np.array([[-1, 0, 1], [0, 0, 0], [1, 0, -1]]))
-cv2.imwrite('edge_detection2.jpg', image_edge2)
+cv2.imwrite(IMAGES_PATH + 'edge_detection2.jpg', image_edge2)
 
 # kernel to be used for box blur
 imageboxblur = convolve2d(input_image, kernel=np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])/9.0)
-cv2.imwrite('box_blur.jpg', imageboxblur)
+cv2.imwrite(IMAGES_PATH + 'box_blur.jpg', imageboxblur)
 
 # kernel to be used for gaussian blur
 imagegaussianblur = convolve2d(input_image, kernel=np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]])/16.0)
-cv2.imwrite('gaussian_blur.jpg', imagegaussianblur)
+cv2.imwrite(IMAGES_PATH + 'gaussian_blur.jpg', imagegaussianblur)
